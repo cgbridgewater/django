57e5ea07c2e3d0eefa95d84ff8f1fe142ae4b2a2
cgbridgewater <cgbridgewater@outlook.com>
2024-01-05 00:05:04 -0800

    correct form template

--- django_intro/get_post/app_one/views.py
+++ django_intro/get_post/app_one/views.py
@@ -8,20 +8,22 @@ def index(request):
 def create_user(request):
     print("Got Post Info...............")
     name_from_form = request.POST["name"]
-    email_from_form = request.POST["email"]
+    dojo_from_form = request.POST["dojo"]
+    language_from_form = request.POST["language"]
     text_from_form = request.POST["text"]
     ninja_from_form = request.POST["ninja"]
     if 'spam' not in request.POST:
         spam_from_form = "No Thanks"
     else:
         spam_from_form = request.POST['spam']
-    print("ninja", ninja_from_form)
+    print("ninja", request.POST)
     print("spam", spam_from_form)
     context = {
         "name_on_template" : name_from_form,
-        "email_on_template" : email_from_form,
         "text_on_template" : text_from_form,
         "ninja_on_template" : ninja_from_form,
-        "spam_on_template" : spam_from_form
+        "spam_on_template" : spam_from_form,
+        "dojo_on_template" : dojo_from_form,
+        "language_on_template" : language_from_form
     }
     return render(request, "results.html", context)
