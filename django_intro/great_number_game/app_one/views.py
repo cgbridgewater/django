296609e1b4c216529490aa7b6b3a59712aab0fc2
cgbridgewater <cgbridgewater@outlook.com>
2024-01-05 22:52:53 -0800

    clean up and comments

--- django_intro/great_number_game/app_one/views.py
+++ django_intro/great_number_game/app_one/views.py
@@ -3,35 +3,37 @@ import random
 
 def index(request):
     # initialize values #
-    guesses = 0
+    attempts = 0
     output = ''
     play_again = False
     game_over = False
     playing = False
-    # if guesses not in session, create guesses session #
-    if 'guesses' in request.session:
-        guesses = request.session['guesses']
+    # if attempts not in session, create attempts session #
+    if 'attempts' in request.session:
+        attempts = request.session['attempts']
     # if magic number not in session, create magic number session #
-    if 'num' in request.session:
-        num = request.session['num']
+    if 'number' in request.session:
+        number = request.session['number']
     # create a random number for the magic number #
     else:
-        num = random.randint(1, 100)
-        request.session['num'] = num
+        number = random.randint(1, 100)
+        request.session['number'] = number
     # console the magic number
-    print(num)
+    print(number)
 
+    # If POST method increase attempts and compare numbers #
     if request.method == 'POST':
-        guesses += 1
-        request.session['guesses'] = guesses
+        attempts += 1
+        request.session['attempts'] = attempts
         playing = True
         guess = request.POST['guess']
-
-        if int(guess) == num:
-            request.session['score'] = guesses
-            del request.session['guesses']
-            del request.session['num']
-            output = f'Winna Winna Chicken Dinna!  {num} was the number'
+        
+        # Winning Scenario #
+        if int(guess) == number:
+            request.session['score'] = attempts
+            del request.session['attempts']
+            del request.session['number']
+            output = f'Winna Winna Chicken Dinna!  {number} was the number'
             play_again = True
             context = {
                 'output': output,
@@ -41,18 +43,24 @@ def index(request):
             }
             return render(request, 'index.html', context)
 
+        # No input Scenario #
         if int(guess) == 0:
-            if guesses < 5:
+            if attempts < 5:
                 output = "No number entered! Turn Lost"
-        elif int(guess) < num:
+        # Low Guess Scenario #
+        elif int(guess) < number:
             output = 'Too Low'
+        # High Guess Scenario #
         else:
             output = 'Too high'
-        if guesses >= 5:
-            del request.session['guesses']
-            del request.session['num']
+        
+        # Straight Outta attempts Scenario #
+        if attempts >= 5:
+            del request.session['attempts']
+            del request.session['number']
             game_over = True
 
+    # context to send to page #
     context = {
         'output': output,
         'play_again': play_again,
@@ -61,20 +69,24 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-# take second element for sort
+# take second element for sorting scoreboard #
 def takeSecond(elem):
     return elem[1]
 
 def scoreboard(request):
-    print("name ", request.POST['name'])
-    print("score ", request.session['score'])
+    # No Leaderboard in session, make it! #
     if not 'leaderboard' in request.session:
         request.session['leaderboard'] = []
+    # If is a POST #
     if request.method == 'POST':
+        # append score to leader board #
         request.session['leaderboard'].append([request.POST['name'], request.session['score']])
+        # sort the leader board #
         request.session['leaderboard'].sort(key= takeSecond )
+        # trim leader board to top 10 #
         request.session['leaderboard']=request.session['leaderboard'][:10]
+        # save it to session #
         request.session.save()
+        # create a current score to post #
         request.session['current_score'] = request.POST['name'], request.session['score']
     return render(request, 'leaders.html')
-
