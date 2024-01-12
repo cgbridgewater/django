99e9ce9fb85313798138d3d6a9dbdd31e1dadad3
cgbridgewater <cgbridgewater@outlook.com>
2024-01-11 22:04:45 -0800

    adding bike widget

---
+++ full_stack_django/dojos_and_ninjas/dojos_and_ninjas_project/dojos_and_ninjas_app/static/js/script.js
@@ -0,0 +1,37 @@
+// <script>
+// javascript
+
+var mouseCurrent = 0;
+var mouseX = 0, mouseY = 0;
+var follower = document.querySelector("#image");
+
+document.addEventListener("mousemove", function(e) {
+mouseX = e.pageX;
+mouseY = e.pageY;
+
+if (mouseX > mouseCurrent) {
+    follower.classList.remove("flipped");
+} else if (mouseX === mouseCurrent) {
+    // no change
+} else {
+    follower.classList.add("flipped");
+}
+
+mouseCurrent = mouseX;
+});
+
+var xp = 0, yp = 0;
+var loop = setInterval(function() {
+xp += (mouseX - xp) / 60; //32
+// yp += (mouseY - yp) / 20; //20
+follower.style.left = xp + "px";
+}, 30);
+
+document.addEventListener("mousedown", function(e) {
+follower.classList.add("jumping");
+});
+
+document.addEventListener("mouseup", function(e) {
+follower.classList.remove("jumping");
+});
+// </script>
